786caf610efd67c51902f2dd23bb68755cc87a95
Mandla <drndlovu@mail.com>
2026-09-12 15:57:18 +0200

    fixes map

---
+++ assets/driver-v2/js/config.js
@@ -0,0 +1,49 @@
+/* ============================================================
+   ASIYE DRIVER V2
+   CONFIGURATION
+   ============================================================ */
+
+window.ASIYE_DRIVER_CONFIG = {
+
+    /*
+     * MAPBOX
+     *
+     * Use your existing PUBLIC Mapbox token.
+     * It should begin with:
+     *
+     * pk.
+     */
+
+    mapboxToken:
+        "pk.eyJ1IjoiYXNpeWUxIiwiYSI6ImNtcWR2dHBydDEyMjIycXF5eThzcWUzcXUifQ.KgsJuS9O1OLDvAN2CMmrKw",
+
+
+    /*
+     * FIREBASE
+     */
+
+    firebase: {
+
+        apiKey:
+            "AIzaSyCX_euO2EEPhfhuG5DTsSi5vCpZ9MFZczY",
+
+        authDomain:
+            "asiye-80386.firebaseapp.com",
+
+        databaseURL:
+            "https://asiye-80386-default-rtdb.firebaseio.com",
+
+        projectId:
+            "asiye-80386",
+
+        storageBucket:
+            "asiye-80386.firebasestorage.app",
+
+        messagingSenderId:
+            "531902350858",
+
+        appId:
+            "1:531902350858:web:f8a4a246bf350d50e3c1f3"
+    }
+
+};
